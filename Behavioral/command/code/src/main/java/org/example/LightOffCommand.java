0bf7f99c9bca5015e009c9f5f25c1bb2951a8f23
Nguyen Kien <nguyntrungkin091@gmail.com>
2024-01-24 17:04:38 +0700

    update command pattern

---
+++ Behavioral/command/code/src/main/java/org/example/LightOffCommand.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class LightOffCommand implements Command{
+    Light light;
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+    @Override
+    public void execute() {
+        light.off();
+    }
+}
